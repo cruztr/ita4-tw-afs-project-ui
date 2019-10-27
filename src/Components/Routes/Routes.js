1db1d46c1e95b975f6ee29f3e303f1fb2b09f940
Mikecrophones07 <mike.toradora07@gmail.com>
2019-10-27 16:32:19 +0800

    [Login-App_User] - create login components and state for loginUser
    - change route for ParkingBoy from '/' to '/parkingBoy'

--- src/Components/Routes/Routes.js
+++ src/Components/Routes/Routes.js
@@ -1,13 +1,15 @@
 import React from "react";
 import {Route,BrowserRouter} from "react-router-dom";
-import LoginContainer from "../../State/ParkingBoy/Login/Container";
+import LoginParkingBoyContainer from "../../State/ParkingBoy/Login/Container";
+import LoginCarOwnerContainer from "../../State/CarOwner/Login/Container";
 import MainPage from "../ParkingBoy/MainPage/MainPage";
 
 export default function Routes() {
     return (
         <BrowserRouter>
         <div>
-            <Route exact path="/" component={LoginContainer} />
+            <Route exact path="/" component={LoginCarOwnerContainer} />
+            <Route exact path="/parkingBoy" component={LoginParkingBoyContainer} />
             <Route exact path="/mainpage" component={MainPage} />
             </div>
         </BrowserRouter>
