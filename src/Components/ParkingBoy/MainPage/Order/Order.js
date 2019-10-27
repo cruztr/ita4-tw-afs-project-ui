02e067ee40a3749b44bd9d723e76653dc605ddab
delatma <delatma@oocl.com>
2019-10-27 17:25:13 +0800

    update UI for homepage

--- src/Components/ParkingBoy/MainPage/Order/Order.js
+++ src/Components/ParkingBoy/MainPage/Order/Order.js
@@ -1,78 +1,84 @@
-import {Input, Button, Row, Modal, Col} from 'antd'
-import 'antd/dist/antd.css';
-import React from 'react';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import React from "react";
+import OrderContainer from "../../../../State/ParkingBoy/MainPage/Order/Container.js";
+import Logo from "../sparkLogo.png";
+import '../MainPage.css';
+const { Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
-export default class Order extends React.Component{
+class MainPage extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            plateNumber: '',
-            parkingLot: 'Scape Building',
-            location: '1st floor',
-            blockNumber: 'My Block',
-            timeIn: '12:04 PM'
-        }
+            collapsed: false,
+        };
     }
-    createOrder = () =>{ 
-        const param = {
-            plateNumber: this.state.plateNumber,
-            parkingLotID: 1,
-            parkingBoyID: 1
-        }
-        this.props.createOrder(param);
-        this.setModal2Visible(false);
-    }
-    state = {
-        modal2Visible: false,
+
+
+    onCollapse = collapsed => {
+        console.log(collapsed);
+        this.setState({ collapsed });
     };
 
-    setModal2Visible(modal2Visible) {
-        this.setState({ modal2Visible });
-    }
-
-    plateNumberChange = (event) => this.setState({plateNumber: event.target.value});
-    render(){
-        return(
-            <div>
-                <Button type="primary" onClick={() => this.setModal2Visible(true)}>
-                    Create Order
-                </Button>
-                <Modal
-                    title="Create Order"
-                    centered
-                    visible={this.state.modal2Visible}
-                    on={() => this.createOrder}
-                    onCancel={() => this.setModal2Visible(false)}
-                 >
-
-                    <Row type="flex" justify="center">
-                        <Col span={8}>Plate Number:</Col>
-                        <Col span={12}><Input placeholder="Plate Number" value={this.state.plateNumber}
-                                              onChange={this.plateNumberChange}/></Col>
-                    </Row>
-                    <Row type="flex" justify="center">
-                        <Col span={8}>Parking Lot:</Col>
-                        <Col span={12}><Input placeholder={this.state.parkingLot} disabled={true}/></Col>
-                    </Row>
-
-                    <Row type="flex" justify="center">
-                        <Col span={8}>Location:</Col>
-                        <Col span={12}><Input placeholder={this.state.location} disabled={true}/></Col>
-                    </Row>
-                    <Row type="flex" justify="center">
-                        <Col span={8}>Blk. Number:</Col>
-                        <Col span={12}><Input placeholder={this.state.blockNumber} disabled={true}/></Col>
-                    </Row>
-                    <Row type="flex" justify="center">
-                        <Col span={8}>Created By:</Col>
-                        <Col span={12}><Input placeholder={this.state.timeIn} disabled={true}/></Col>
-                    </Row>
-                    <Row type="flex" justify="center" className="row">
-                        <Button className="Create-Order" onClick={this.createOrder}>Create Order</Button>
-                        <Button className="Cancel" >Cancel</Button>
-                    </Row>
-                </Modal>
-            </div>
+    render() {
+        return (
+            <Layout style={{minHeight: '100vh' }}>
+                <Sider className="slider" >
+                    <div className="logo"><img width="200px" alt="Spark" src={Logo}/></div>
+                    <Menu theme="light" defaultSelectedKeys={['1']} mode="inline">
+                        <SubMenu
+                            key="sub1"
+                            title={
+                                <span>
+                  <Icon type="car" />
+                  <span>Parking Lots</span>
+                </span>
+                            }
+                        >
+                            <Menu.Item key="2">Orders</Menu.Item>
+                            <Menu.Item key="3">Reservations</Menu.Item>
+                        </SubMenu>
+                        <Menu.Item key="4">
+                            <Icon type="file" />
+                            <span>Logs</span>
+                        </Menu.Item>
+                        <Menu.Item key="5">
+                            <Icon type="info-circle" />
+                            <span>About Spark</span>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    {/*<Header style={{ background: '#fff', padding: 0 }} />*/}
+                    <Content style={{ margin: '0 16px' }}>
+                        <Breadcrumb style={{ margin: '16px 0' }}>
+                            <Breadcrumb.Item>Parking Lots</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+                            <OrderContainer Mac />
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>spark ©2019 Created by EUTeam</Footer>
+                </Layout>
+            </Layout>
         );
     }
 }
+
+export default MainPage;
+
+
+// import React from "react";
+// import OrderContainer from "../../../State/ParkingBoy/MainPage/Order/Container.js";
+//
+// class MainPage extends React.Component{
+//     render(){
+//         return(
+//             <div className="parkingboy-main-page">
+//                 <OrderContainer />
+//             </div>
+//         );
+//     }
+// }
+//
+// export default MainPage;
