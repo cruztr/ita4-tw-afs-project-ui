543819930ef7193507aff4e812961eb98713c53d
GENREV ARAMBULO (SAPE-MIS-ISD-OOCLL/MNL) <ARAMBGE@OOCL.COM>
2019-10-27 16:00:59 +0800

    [VIEW RESERVATIONS] - Added search function for name and plate number

--- src/Components/ParkingBoy/MainPage/Reservations/Reservations.js
+++ src/Components/ParkingBoy/MainPage/Reservations/Reservations.js
@@ -1,4 +1,5 @@
-import {Divider, Popconfirm, Table} from 'antd'
+import {Button, Divider, Icon, Input, Popconfirm, Table} from 'antd'
+import Highlighter from 'react-highlight-words';
 import 'antd/dist/antd.css';
 import React from 'react';
 import ReservationResource from "../../../../Api/ReservationResource";
@@ -9,6 +10,7 @@ export default class Reservations extends React.Component{
     constructor(props){
         super(props);
         this.state = {
+            searchText: '',
             columns: [
                 {
                     title: "Reservation Number.",
@@ -18,12 +20,14 @@ export default class Reservations extends React.Component{
                 {
                     title: "Driver Name",
                     dataIndex: "fullName",
-                    key: "fullName"
+                    key: "fullName",
+                    ...this.getColumnSearchProps('fullName')
                 },
                 {
                     title: "Plate Number",
                     dataIndex: "plateNumber",
-                    key: "plateNumber"
+                    key: "plateNumber",
+                    ...this.getColumnSearchProps('plateNumber')
                 },
                 {
                     title: "Reserved Time",
@@ -53,6 +57,66 @@ export default class Reservations extends React.Component{
             ]
         }
     }
+
+    getColumnSearchProps = dataIndex => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+            <div style={{ padding: 8 }}>
+                <Input
+                    ref={node => {
+                        this.searchInput = node;
+                    }}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
+                    style={{ width: 188, marginBottom: 8, display: 'block' }}
+                />
+                <Button
+                    type="primary"
+                    onClick={() => this.handleSearch(selectedKeys, confirm)}
+                    icon="search"
+                    size="small"
+                    style={{ width: 90, marginRight: 8 }}
+                >
+                    Search
+                </Button>
+                <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+                    Reset
+                </Button>
+            </div>
+        ),
+        filterIcon: filtered => (
+            <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex]
+                .toString()
+                .toLowerCase()
+                .includes(value.toLowerCase()),
+        onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+                setTimeout(() => this.searchInput.select());
+            }
+        },
+        render: text => (
+            <Highlighter
+                highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+                searchWords={[this.state.searchText]}
+                autoEscape
+                textToHighlight={text.toString()}
+            />
+        ),
+    });
+
+    handleSearch = (selectedKeys, confirm) => {
+        confirm();
+        this.setState({ searchText: selectedKeys[0] });
+    };
+
+    handleReset = clearFilters => {
+        clearFilters();
+        this.setState({ searchText: '' });
+    };
 
     componentDidMount(){
         ReservationResource.getAllReservation()
