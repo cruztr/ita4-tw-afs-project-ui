cdb6e396ea635b9588daf212ed791c4b9d340e52
MARK ANTHONY DE LA TORRE (GEDI-ISD-OOCLL/MNL) <DELATMA@OOCL.COM>
2019-10-28 19:07:33 +0800

    update LOGIN page UI

--- src/Components/Accounts/Login/Login.js
+++ src/Components/Accounts/Login/Login.js
@@ -1,9 +1,12 @@
 import React from "react";
-import {Button, Card, Input} from "antd";
+import {Button, Card, Input, Layout, Row, Col } from "antd";
 import 'antd/dist/antd.css';
 import './Login.css';
-import sparkImage from './Images/route.png';
+import sparkImage from './Images/logowhitebordered.png';
+import driveArriveRelax from './Images/driverarriverelax.png';
 import { Redirect, Link } from 'react-router-dom'
+const { Header, Footer, Sider, Content } = Layout;
+const { Search } = Input;
 
 class Login extends React.Component{
     constructor(props){
@@ -53,48 +56,75 @@ class Login extends React.Component{
     }
 
     render(){
-        return( 
-            <div className="Login-CheckOrder_Whole">
-                {this.goLogin()}
-                {this.checkOrder()}
-                <div className="UserContainer">
-                    <Card className="Login-CheckOrder">
-                        <Card className= "Login-Logo" bordered={false}
-                              cover={<img alt="Spark" src={sparkImage} />}>
-                        </Card>
-                        <div className="Login-CheckOrder-Input">
-                            <p>Check Order Status</p>
-                            <Input placeholder="Order Id" value={this.state.orderId} name="orderId"
-                                   onChange={this.handleChange}/>
-                            <Button onClick = {this.handleCheckOrder}>Submit</Button>
+        return(
+            <div className={"maindiv"}>
+                <Layout className={"Layout"}>
+                    <div className={"Content"}>
+                        <div className={"wrapper"}>
+                            <Content>
+                                {this.goLogin()}
+                                {this.checkOrder()}
+                                {/*Check Order*/}
+                                <div className={"es1"}>
+                                    <div className={"Order-block"}>
+                                        <Card className={"st1"} bordered={false}>
+                                            <p className={"Ps1"}>PARKING JUST GOT A LOT SIMPLER</p>
+                                            <Card bordered={false}
+                                                  cover={<img alt="Spark" src={driveArriveRelax}/>}>
+                                            </Card>
+
+                                            {/*<div>*/}
+                                            {/*    <Input placeholder="Order Id" value={this.state.orderId} name="orderId"*/}
+                                            {/*           onChange={this.handleChange}/>*/}
+                                            {/*    <Button onClick = {this.handleCheckOrder}>Submit</Button>*/}
+                                            {/*</div>*/}
+                                            <p className={"pSm"}>Check the status of your parked car:</p>
+                                            <Search
+                                                value={this.state.orderId} name="orderId"
+                                                onChange={this.handleChange}
+                                                placeholder="Input Order Number"
+                                                enterButton="Search"
+                                                size="large"
+                                                onSearch={this.handleCheckOrder}
+                                            />
+                                        </Card>
+                                    </div>
+                                </div>
+                                {/*login*/}
+                            <div className={"es2"}>
+                                    <div className={"Login-block"}>
+                                        <Card className={"Login-card"} bordered={false}
+                                              cover={<img alt="Spark" src={sparkImage}/>}
+                                                  >
+                                           <div className={"Input"}>
+                                                <Input className={"Login-uform"} placeholder="Username" value={this.state.username}
+                                                   name="username"
+                                                   onChange={this.handleChange}/>
+                                           </div>
+                                           <div className={"Input"}>
+                                            <Input className={"Login-uform"} placeholder="Password" type="password" value={this.state.password}
+                                                   name="password"
+                                                   onChange={this.handleChange}/>
+                                           </div>
+                                            <div>
+                                                <div className={"Div-btn"}>
+                                                    <Button className={"User-btn"} onClick = {this.setUserCarOwner}>I am a Car Owner</Button>
+                                                </div>
+                                                <div className={"Div-btn"}>
+                                                    <Button className={"Boy-btn"} onClick = {this.setUserParkingBoy}>I am a Parking Boy</Button>
+                                                </div>
+                                                <p className={"Ps2"}>Register as Car Owner? <Link to="/signUp">Click here</Link></p>
+                                            </div>
+                                        </Card>
+                                    </div>
+                                </div>
+                            </Content>
+                            <Footer className={"Footer"}>Created by EUTeam</Footer>
                         </div>
-                    </Card>
-                    <Card className="Login-CheckOrder">
-                        <Card className= "Login-Logo" bordered={false}
-                              cover={<img alt="Spark" src={sparkImage} />}>
-                        </Card>
-                        <div className="Login-CheckOrder-Input">
-                            <Input placeholder="Username" value={this.state.username} name="username"
-                                   onChange={this.handleChange}/>
-                            <Input placeholder="Password" type="password" value={this.state.password} name="password"
-                                   onChange={this.handleChange}/>
-                            <p className="Sign-Up">Register as Car Owner? <Link to="/signUp">Click here</Link></p>
-                            <div className="UserContainer">
-                                <Button onClick = {this.setUserCarOwner}>Login As Car Owner</Button>
-                                <Button onClick = {this.setUserParkingBoy}>Login As Parking Boy</Button>
-                            </div>
-                        </div>
-                    </Card>
-                </div>
-
-
-                {/*<Card  className="Login">*/}
-                {/*</Card>*/}
-                {/*<Card  className="Login">*/}
-
-                {/*</Card>*/}
+                    </div>
+                </Layout>
             </div>
-        );
+    );
     }
 }
 
