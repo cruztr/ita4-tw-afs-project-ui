cbb2b598585b4dfc4ec397536516c4bd8265aba7
Mikecrophones07 <mike.toradora07@gmail.com>
2019-10-28 14:59:04 +0800

    [CHECK-ORDER/LOGIN] - remove Login container and state from parkingBoy and CarOwner

--- src/Components/Routes/Routes.js
+++ src/Components/Routes/Routes.js
@@ -1,7 +1,5 @@
 import React from "react";
 import {Route,BrowserRouter} from "react-router-dom";
-import LoginParkingBoyContainer from "../../State/Accounts/Login/Container";
-import LoginCarOwnerContainer from "../../State/CarOwner/Login/Container";
 import AccountsContainer from "../../State/Accounts/Login/Container"
 import SignUpCarOwnerContainer from "../../State/CarOwner/Signup/Container"
 import MainPage from "../ParkingBoy/MainPage/MainPage";
@@ -12,9 +10,7 @@ export default function Routes() {
     return (
         <BrowserRouter>
         <div>
-            {/*<Route exact path="/" component={LoginCarOwnerContainer} />*/}
             <Route exact path="/" component={AccountsContainer} />
-            <Route exact path="/parkingBoy" component={LoginParkingBoyContainer} />
             <Route exact path="/signUp" component={SignUpCarOwnerContainer} />
             <Route exact path="/mainpage" component={MainPage} />
             <Route exact path="/mainpageCarOwner" component={MainPageCarOwner} />
--- src/State/RootReducer.js
+++ src/State/RootReducer.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
-import loginReducer from "./ParkingBoy/Login/Reducer.js";
 import orderReducer from "./ParkingBoy/MainPage/Order/Reducer.js";
 import reservationReducer from "./ParkingBoy/MainPage/Reservations/Reducer.js";
 import signUpReducer from "./CarOwner/Signup/Reducer.js";
 import accountsReducer from "./Accounts/Login/Reducer.js";
 
 export default combineReducers({
-    loginReducer,
     orderReducer,
     reservationReducer,
     signUpReducer,
