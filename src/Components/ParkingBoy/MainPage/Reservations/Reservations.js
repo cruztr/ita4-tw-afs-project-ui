6895fe48ff17b84dfa012f73a868b39b34b552e2
GENREV ARAMBULO (SAPE-MIS-ISD-OOCLL/MNL) <ARAMBGE@OOCL.COM>
2019-10-28 12:26:37 +0800

    [VIEW RESERVATIONS] - Merged with the sparks dev.
    - Added functionality to create order via Reservations.

--- src/Components/ParkingBoy/MainPage/Reservations/Reservations.js
+++ src/Components/ParkingBoy/MainPage/Reservations/Reservations.js
@@ -1,11 +1,11 @@
-import {Button, Divider, Icon, Input, Popconfirm, Table} from 'antd'
+import {Button, Icon, Input, Popconfirm, Table} from 'antd'
 import Highlighter from 'react-highlight-words';
 import 'antd/dist/antd.css';
 import React from 'react';
 import ReservationResource from "../../../../Api/ReservationResource";
 
 export default class Reservations extends React.Component{
-
+    intervalID;
 
     constructor(props){
         super(props);
@@ -36,20 +36,30 @@ export default class Reservations extends React.Component{
                 },
                 {
                     title: "Action",
-                    // dataIndex: "reservedTime",
                     key: "action",
                     render: (text,reservation) => (
                         <span>
                             <Popconfirm title="Sure to confirm?" onConfirm={() => {
-                                console.log(reservation);
+                                {this.createOrder(reservation)}
                             }}>
                                 <a>Confirm</a>
+
                             </Popconfirm>
                         </span>
                     )
                 }
             ]
         }
+    }
+
+    createOrder = reservation => {
+        const param = {
+            parkingLotID: reservation.parkingLot.parkingLotID,
+            parkingBoyID: this.props.parkingBoy.parkingBoyID,
+            parkingBlockPosition: reservation.parkingBlockPosition,
+            reservation: reservation
+        }
+        this.props.createOrder(param);
     }
 
     getColumnSearchProps = dataIndex => ({
@@ -113,8 +123,19 @@ export default class Reservations extends React.Component{
     };
 
     componentDidMount(){
+        this.getData();
+    }
+
+    getData = () => {
         ReservationResource.getAllReservation()
-        .then(res => res.json()).then(res => this.props.refreshContent(res));
+            .then(res => res.json()).then(res => {
+            this.props.refreshContent(res);
+            this.intervalID = setTimeout(this.getData.bind(this), 5000);
+        });
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.intervalID);
     }
 
     render(){
